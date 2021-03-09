9c4815f691181e020025a7ff24f688eedecf1933
richard <richard_marllon@hotmail.com>
2021-03-09 18:35:26 -0300

    BREAKING CHANGE: extends state to whole app using ContextAPI

---
+++ src/Providers/User/index.js
@@ -0,0 +1,15 @@
+const { createContext, useContext, useState } = require("react");
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+	const [user, setUser] = useState([]);
+
+	return (
+		<UserContext.Provider value={{ user, setUser }}>
+			{children}
+		</UserContext.Provider>
+	);
+};
+
+export const useUser = () => useContext(UserContext);
