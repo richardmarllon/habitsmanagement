f63085e5f5bb873f5e8388d8f8da705abc861dfc
richard <richard_marllon@hotmail.com>
2021-03-10 18:41:02 -0300

    feat: add personal habits list through API comunication

--- src/Providers/Habits/index.js
+++ src/Providers/Habits/index.js
@@ -1,12 +1,31 @@
-const { createContext, useContext, useState } = require("react");
+import { habitsAPI } from "../../services/api";
+const { createContext, useContext, useState, useEffect } = require("react");
 
 const HabitsContext = createContext();
 
 export const HabitsProvider = ({ children }) => {
 	const [habits, setHabits] = useState([]);
+	const [personalHabits, setPersonalHabits] = useState([]);
+
+	const token = localStorage.getItem("token") || ""; //after insert Token State, need to change  this token below to Token State
+
+	useEffect(() => {
+		if (token !== "") {
+			const AuthConfig = { Authorization: `Bearer ${token}` };
+			async function GetPersonalHabits() {
+				const response = await habitsAPI.get(`habits/personal/`, {
+					headers: AuthConfig,
+				});
+				setPersonalHabits(response.data);
+			}
+			GetPersonalHabits();
+		}
+	}, []);
 
 	return (
-		<HabitsContext.Provider value={{ habits, setHabits }}>
+		<HabitsContext.Provider
+			value={{ habits, setHabits, personalHabits, setPersonalHabits }}
+		>
 			{children}
 		</HabitsContext.Provider>
 	);
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,17 @@
+import { useHabits } from "../../Providers/Habits";
+
 const Home = () => {
+	const { personalHabits } = useHabits();
+
 	return (
 		<div>
 			<h1>Home</h1>
+			<ul>
+				Lista de habitos pessoais:
+				{personalHabits.map((item) => {
+					return <li key={item.id}>{item.title}</li>;
+				})}
+			</ul>
 		</div>
 	);
 };
