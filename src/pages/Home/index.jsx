5cde696b0ef5ec84667cc2c8bd4a68f4a32fa3a6
richard <richard_marllon@hotmail.com>
2021-03-10 18:54:02 -0300

    feat: add activities list through API comunication

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,23 @@
+import { useActivities } from "../../Providers/Activities";
+
 const Home = () => {
+	const { activities } = useActivities();
+
 	return (
 		<div>
 			<h1>Home</h1>
+			<div>
+				<ul>
+					Lista de atividades:
+					{activities.map((item) => {
+						return (
+							<li key={item.id}>
+								{item.title} do grupo {item.group}
+							</li>
+						);
+					})}
+				</ul>
+			</div>
 		</div>
 	);
 };
